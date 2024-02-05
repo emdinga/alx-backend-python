36f9f8a1146d82a35492feb9f3670cbbc29ab1cb
Emdinga <emdinga@gmail.com>
2024-02-05 06:29:21 -0800

    testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -25,15 +25,19 @@ class TestGithubOrgClient(unittest.TestCase):
         org = GithubOrgClient("test")
         org._public_repos_url = 'https://api.github.com/orgs/test/repos'
         mock_get_json.return_value = [{'name': 'repo1'}, {'name': 'repo2'}]
-        self.assertEqual(org._public_repos_url, 'https://api.github.com/orgs/test/repos')
+        self.assertEqual(
+                org._public_repos_url,
+                'https://api.github.com/orgs/test/repos')
 
     @patch('client.get_json')
     @patch('client.GithubOrgClient._public_repos_url')
     def test_public_repos(self, mock_public_repos_url, mock_get_json):
         mock_get_json.return_value = [{'name': 'repo1'}, {'name': 'repo2'}]
-        mock_public_repos_url.return_value = 'https://api.github.com/orgs/test/repos'
+        mock_public_repos_url.return_value =
+        'https://api.github.com/orgs/test/repos'
         org = GithubOrgClient("test")
-        self.assertEqual(org.public_repos(), [{'name': 'repo1'}, {'name': 'repo2'}])
+        self.assertEqual(org.public_repos(),
+                [{'name': 'repo1'}, {'name': 'repo2'}])
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
@@ -43,15 +47,17 @@ class TestGithubOrgClient(unittest.TestCase):
         org = GithubOrgClient("test")
         self.assertEqual(org.has_license(repo, license_key), expected_result)
 
-@parameterized_class(('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'), [
+
+@parameterized_class(('org_payload', 'repos_payload',
+    'expected_repos', 'apache2_repos'), [
     ({}, {}, [], []),
     ({"repos_url": "https://api.github.com/orgs/test/repos"}, {}, [], []),
     ({}, {"name": "repo1"}, [{"name": "repo1"}], []),
-    ({}, {"name": "repo1", "license": {"key": "apache-2.0"}}, [], [{"name": "repo1"}]),
+    ({}, {"name": "repo1", "license": {"key": "apache-2.0"}},
+        [], [{"name": "repo1"}]),
 ])
 
 class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """ test github"""
     @classmethod
     def setUpClass(cls):
         cls.get_patcher = patch('client.requests.get')
@@ -73,8 +79,5 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         org.org_payload = self.org_payload
         org.repos_payload = self.repos_payload
         self.mock_get().json.side_effect = [org.org_payload, org.repos_payload]
-        self.assertEqual(org.public_repos(license="apache-2.0"), self.apache2_repos)
-
-if __name__ == '__main__':
-    unittest.main()
-
+        self.assertEqual(org.public_repos(
+            license="apache-2.0"), self.apache2_repos)
