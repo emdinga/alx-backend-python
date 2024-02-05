bd8f76c9ae3c80c27d7aa27c8337691b8a2acd74
Emdinga <emdinga@gmail.com>
2024-02-05 00:38:48 -0800

    unit testing

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+""" test client"""
+
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized, parameterized_class
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ test git hub"""
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        mock_get_json.return_value = {}
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, {})
+
+    @patch('client.get_json')
+    def test_public_repos_url(self, mock_get_json):
+        org = GithubOrgClient("test")
+        org._public_repos_url = 'https://api.github.com/orgs/test/repos'
+        mock_get_json.return_value = [{'name': 'repo1'}, {'name': 'repo2'}]
+        self.assertEqual(org._public_repos_url, 'https://api.github.com/orgs/test/repos')
+
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url')
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        mock_get_json.return_value = [{'name': 'repo1'}, {'name': 'repo2'}]
+        mock_public_repos_url.return_value = 'https://api.github.com/orgs/test/repos'
+        org = GithubOrgClient("test")
+        self.assertEqual(org.public_repos(), [{'name': 'repo1'}, {'name': 'repo2'}])
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        org = GithubOrgClient("test")
+        self.assertEqual(org.has_license(repo, license_key), expected_result)
+
+@parameterized_class(('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'), [
+    ({}, {}, [], []),
+    ({"repos_url": "https://api.github.com/orgs/test/repos"}, {}, [], []),
+    ({}, {"name": "repo1"}, [{"name": "repo1"}], []),
+    ({}, {"name": "repo1", "license": {"key": "apache-2.0"}}, [], [{"name": "repo1"}]),
+])
+
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ test github"""
+    @classmethod
+    def setUpClass(cls):
+        cls.get_patcher = patch('client.requests.get')
+        cls.mock_get = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        org = GithubOrgClient("test")
+        org.org_payload = self.org_payload
+        org.repos_payload = self.repos_payload
+        self.mock_get().json.side_effect = [org.org_payload, org.repos_payload]
+        self.assertEqual(org.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        org = GithubOrgClient("test")
+        org.org_payload = self.org_payload
+        org.repos_payload = self.repos_payload
+        self.mock_get().json.side_effect = [org.org_payload, org.repos_payload]
+        self.assertEqual(org.public_repos(license="apache-2.0"), self.apache2_repos)
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Unit test"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError, "Key 'a' not found in the nested map"),
+        ({"a": 1}, ("a", "b"), KeyError,
+            "Key 'b' not found in the nested map"),
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_exception, expected_message):
+        with self.assertRaises(expected_exception) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected_message)
