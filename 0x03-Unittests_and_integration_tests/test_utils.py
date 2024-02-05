3effde2b617b31b50f252b166941f69a2f8d1ad8
Emdinga <emdinga@gmail.com>
2024-02-05 06:16:09 -0800

    unit testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,11 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({}, ("a",), KeyError, "Key 'a' not found in the nested map"),
-        ({"a": 1}, ("a", "b"), KeyError,
-            "Key 'b' not found in the nested map"),
+        ({"a": 1}, ("a", "b"), KeyError, "Key 'b' not found in the nested map"),
     ])
     def test_access_nested_map_exception(
             self, nested_map, path, expected_exception, expected_message):
         with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_message)
+        
+        actual_message = str(context.exception)
+        self.assertIn(expected_message, actual_message)
